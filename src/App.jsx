c7280fcfdcd3c837319e1e576a9cf50b8f6aa0e6
Henning Valderhaug <hvalderhaug@gmail.com>
2024-06-04 21:24:15 +0200

    Adds initial css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import { useState } from "react";
 
-import "./App.css";
+import styles from "./App.module.css";
+
+// add simple input on left and a textbox on the right that simply displays the inputted data again
 
 export default function App() {
-  return <>hello</>;
+  const [firstText, setFirstText] = useState("Nothing added yet");
+  const [showLetter, setShowLetter] = useState(true);
+
+  return (
+    <div className={styles.mainContent}>
+      <div className={styles.inputsField}>
+        <input
+          type="text"
+          placeholder="hello"
+          onChange={(e) => {
+            e.preventDefault;
+            setFirstText(e.target.value);
+          }}
+        />
+        <button onClick={() => setShowLetter(!showLetter)}>Show letter</button>
+      </div>
+
+      {/* OUTPUT FIELDS */}
+      <div className={styles.outputsField}>
+        <div className={styles.notesField}>
+          <p className={styles.content}>another line of content</p>
+        </div>
+
+        <div className={styles.letterField}>
+          {showLetter && <p className={styles.content}>{firstText}</p>}
+        </div>
+      </div>
+    </div>
+  );
 }
