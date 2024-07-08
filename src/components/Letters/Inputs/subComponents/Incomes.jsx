b72c644e6aaab9d09584d72957a9ee0618748d47
Henning Valderhaug <hvalderhaug@gmail.com>
2024-07-08 22:03:23 +0200

    Add newIncomes branch for alt approach

--- src/components/Letters/Inputs/subComponents/Incomes.jsx
+++ src/components/Letters/Inputs/subComponents/Incomes.jsx
@@ -15,42 +15,6 @@ export default function Incomes({ incomes, onChangeContent }) {
     <div>
       {/* NEW VERSION */}
       <h4>Inntekter</h4>
-
-      <ul>
-        {incomes &&
-          incomes.map((income) => (
-            <li key={income.name}>
-              <input
-                placeholder={income.type || "type"}
-                onChange={(e) => {
-                  income.type = e.target.value;
-                  onChangeContent((incomes) => {
-                    return {
-                      incomes: [...incomes],
-                    };
-                  });
-                }}
-              />
-              <input placeholder={income.name || "navn"} />
-              <input placeholder={income.amount || "beløp"} />
-            </li>
-          ))}
-      </ul>
-
-      <button
-        type="button"
-        onClick={() => {
-          onChangeContent(
-            incomes
-              ? {
-                  incomes: [...incomes, {}],
-                }
-              : { incomes: [{}] }
-          );
-        }}
-      >
-        Legg til ytelse
-      </button>
     </div>
   );
 }
